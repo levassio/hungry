6a81f5e55e2e14566400c3f3dfd113ee7007dcaa
levassio <sergiy.yeskov@yahoo.com>
2014-12-27 13:00:39 +1100

    client.resources

--- client/app/client.resources.js
+++ client/app/client.resources.js
@@ -3,4 +3,7 @@
 angular.module('hungryApp')
   .factory('Dish', ['$resource', function ($resource) {
     return $resource('/api/dishes/:dishId', { dishID: '@id' });
+  }])
+  .factory('Order', ['$resource', function ($resource) {
+    return $resource('/api/orders/:orderId', { orderID: '@id' });
   }]);
--- client/app/orders/order.client.controller.js
+++ client/app/orders/order.client.controller.js
@@ -1,37 +1,22 @@
 'use strict';
 
 angular.module('hungryApp')
-  .controller('OrdersCtrl', function (Auth, $http) {
+  .controller('OrdersCtrl', function (Auth, Order, Dish) {
     var cl = this;
 
-    cl.newOrder = {};
-    cl.orders = [];
-    cl.dishes = [];
-
-    function reloadOrders() {
-      $http.get('/api/orders').then(function success(results) {
-        cl.orders = results.data;
-      });
-    }
-
-    function reloadDishes() {
-      $http.get('/api/dishes').then(function success(results) {
-        cl.dishes = results.data;
-        reloadOrders();
-      });
-    }
-
-    reloadDishes();
+    cl.newOrder = new Order();
+    cl.orders = Order.query();
+    cl.dishes = Dish.query();
 
     cl.save = function () {
       cl.newOrder._user = Auth.getCurrentUser()._id;
 
-      $http
-        .post('api/orders',cl.newOrder)
-        .then(function success() {
-          cl.newOrder = {};
-          reloadOrders();
-        });
+      cl.newOrder.$save(function () {
+        cl.orders.push(cl.newOrder);
+        cl.newOrder = new Order();
+      });
+
+      cl.orders = Order.query();
       return false;
     };
 
